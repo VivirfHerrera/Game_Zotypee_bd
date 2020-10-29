f02849d00d99d758e1ca8a1e0e5c79273a42ff80
VivirfHerrera <58118055+VivirfHerrera@users.noreply.github.com>
2020-10-29 13:44:40 -0700

    Agregar archivos de proyecto.

---
+++ Game_Zotypee_bd/Pcarga.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace Game_Zotypee_bd
+{
+    partial class Pcarga
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Pcarga));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.CargaBar = new System.Windows.Forms.ProgressBar();
+            this.timer1 = new System.Timers.Timer();
+            this.trespuntos = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.timer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-2, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(640, 366);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.UseWaitCursor = true;
+            // 
+            // CargaBar
+            // 
+            this.CargaBar.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.CargaBar.Enabled = false;
+            this.CargaBar.Location = new System.Drawing.Point(116, 273);
+            this.CargaBar.Name = "CargaBar";
+            this.CargaBar.Size = new System.Drawing.Size(423, 30);
+            this.CargaBar.TabIndex = 1;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 60D;
+            this.timer1.SynchronizingObject = this;
+            this.timer1.Elapsed += new System.Timers.ElapsedEventHandler(this.timer1_Elapsed);
+            // 
+            // trespuntos
+            // 
+            this.trespuntos.AutoSize = true;
+            this.trespuntos.Enabled = false;
+            this.trespuntos.Location = new System.Drawing.Point(317, 316);
+            this.trespuntos.Name = "trespuntos";
+            this.trespuntos.Size = new System.Drawing.Size(16, 13);
+            this.trespuntos.TabIndex = 5;
+            this.trespuntos.Text = "...";
+            this.trespuntos.Visible = false;
+            // 
+            // Pcarga
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(632, 364);
+            this.Controls.Add(this.trespuntos);
+            this.Controls.Add(this.CargaBar);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Pcarga";
+            this.Text = "Zootype";
+            this.Load += new System.EventHandler(this.Pcarga_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.timer1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ProgressBar CargaBar;
+        private System.Timers.Timer timer1;
+        private System.Windows.Forms.Label trespuntos;
+    }
+}
+
---
+++ Game_Zotypee_bd/Pcarga.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Game_Zotypee_bd
+{
+    public partial class Pcarga : Form
+    {
+        public Pcarga()
+        {
+            InitializeComponent();
+        }
+
+        private void Pcarga_Load(object sender, EventArgs e)
+        {
+
+            trespuntos.Parent = pictureBox1;
+            trespuntos.BackColor = Color.Transparent;
+        }
+        public void Provarcarga()
+        {
+            CargaBar.Increment(1);
+            trespuntos.Text = CargaBar.ToString() + "%";
+            if (CargaBar.Value == CargaBar.Maximum)
+            {
+                timer1.Stop();
+                this.Hide();
+                PMenu ff = new PMenu();
+                ff.Show();
+
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void timer1_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            Provarcarga();
+        }
+    }
+}
